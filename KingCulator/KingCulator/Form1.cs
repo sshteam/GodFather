d2809dfb01656a21f9bd0b361983aaf93a05e8d0
ritaklep <kokokohitaco@gmail.com>
2016-07-11 11:15:34 +0600

    second part

--- KingCulator/KingCulator/Form1.cs
+++ KingCulator/KingCulator/Form1.cs
@@ -16,5 +16,42 @@ namespace KingCulator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(textBox1.Text);
+            double secondValue = Convert.ToDouble(textBox2.Text);
+            double answer = firstValue + secondValue;
+            textBox3.Text = Convert.ToString(answer);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(textBox1.Text);
+            double secondValue = Convert.ToDouble(textBox2.Text);
+            double answer = firstValue - secondValue;
+            textBox3.Text = Convert.ToString(answer);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(textBox1.Text);
+            double secondValue = Convert.ToDouble(textBox2.Text);
+            double answer = firstValue * secondValue;
+            textBox3.Text = Convert.ToString(answer);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double firstValue = Convert.ToDouble(textBox1.Text);
+            double secondValue = Convert.ToDouble(textBox2.Text);
+            double answer = firstValue / secondValue;
+            textBox3.Text = Convert.ToString(answer);
+        }
     }
 }
