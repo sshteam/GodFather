3ea4eb73e1d42eeeb57e6cf671dbc7409adddd11
ritaklep <kokokohitaco@gmail.com>
2016-07-12 13:46:45 +0600

    teat factory

---
+++ KingCulator/KingCulator.Tests/FactoryTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using KingCulator.TwoArgument;
+using NUnit.Framework;
+
+namespace KingCulator.Tests
+{
+    [TestFixture]
+    class FactoryTest
+    {
+        [TestCase("summa", typeof(Summa))]
+
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentFactory.CreateCalculater(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+    }
+}
